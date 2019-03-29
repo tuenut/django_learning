82868d86ce5592de8919e608273c814ee988672c
Groshev Artem <groshev41273m@gmail.com>
2019-03-29 19:39:51 +0300

    pass poll page

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.polls_list, name='polls.index'),
+    path('poll/<int:poll_id>', views.poll_page, name='polls.poll_page')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from polls.models import Poll
 
@@ -6,3 +6,7 @@ from polls.models import Poll
 def polls_list(request):
     context = {'polls_list': Poll.objects.all()}
     return render(request, 'polls/list.html', context)
+
+def poll_page(request, poll_id):
+    context = {'poll': get_object_or_404(Poll, pk=poll_id)}
+    return render(request, 'polls/poll_page.html', context)
